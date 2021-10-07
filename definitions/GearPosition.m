752fdc006bd1554d03cf0de99189552c699f8293
FannoFlow <davidcoventry6@gmail.com>
2021-10-06 22:20:12 -0400

    initial repo

---
+++ definitions/GearPosition.m
@@ -0,0 +1,6 @@
+classdef GearPosition < Simulink.IntEnumType
+  enumeration
+    up   (0)
+    down (1)
+  end
+end
---
+++ definitions/PumpState.m
@@ -0,0 +1,6 @@
+classdef PumpState < Simulink.IntEnumType
+  enumeration
+    off   (0)
+    on    (1)
+  end
+end
